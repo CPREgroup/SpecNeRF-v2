ff0ecbd64f5673f564fc8b4d0f7b939a2adf63de
TIMESTICKING <731908970@qq.com>
2023-04-30 00:29:18 +0800

    not working

--- models/tensorBase.py
+++ models/tensorBase.py
@@ -152,12 +152,14 @@ class SSFFcn(torch.nn.Module):
             # nn.Linear(in_features=2*L, out_features=2*L),
             # nn.LeakyReLU(inplace=True),
             nn.Linear(in_features=2*L, out_features=out_dim),
-            nn.Sigmoid()
+            nn.ReLU(inplace=True)
         )
 
     def forward(self, x):
 
         y = self.layers(x)
+
+        y = y / y.max()
 
         return y
 
