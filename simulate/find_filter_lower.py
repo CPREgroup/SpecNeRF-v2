6ce57a285f19310ccdec9abea34af4a1e520993e
TIMESTICKING <731908970@qq.com>
2023-06-09 11:19:32 +0800

    old find lower dont support bp

--- simulate/find_filter_lower.py
+++ simulate/find_filter_lower.py
@@ -13,7 +13,6 @@ from glob import glob
 from filesort_int import sort_file_int
 from tqdm import tqdm
 from myutils import myutils
-from find_intersection import get_intersection_dist
 
 
 class FindFilter(nn.Module):
@@ -21,7 +20,8 @@ class FindFilter(nn.Module):
     def __init__(self, path) -> None:
         super(FindFilter, self).__init__()
 
-        self.fs = sort_file_int(f'{path}/*.mat', 'mat')[1:]
+        # self.fs = sort_file_int(f'{path}/*.mat', 'mat')[1:]
+        self.fs = glob(f'{path}/*.mat')[1:]
         filters = list(map(lambda x: np.diagonal(sio.loadmat(x)['filter']), self.fs))
         self.filters = torch.from_numpy(np.row_stack(filters)).cuda()
         self.indicator = nn.Parameter(torch.rand([len(filters), 1]))
@@ -34,9 +34,33 @@ class FindFilter(nn.Module):
         return self.filters, indicator
 
 
-def get_intersection(filters):
-    # x, y = get_intersection_dist(filters)
-    # lostx = list(set(range(0, filters.shape[1] - 1)).difference(x))
+from collections import Counter
+
+def findINgroup(v1, g):
+    minus = torch.where((v1 - g) >= 0, torch.tensor(1).cuda(), torch.tensor(0).cuda())
+    diff = minus[:, 1:] - minus[:, :-1]
+    inter_id = torch.nonzero(diff != 0)
+    if inter_id.shape[0] == 0:
+        return False
+    else:
+        return inter_id[:, 1]
+
+
+def get_intersection_dist(filters):
+    xs = []
+    for i in range(filters.shape[0] - 1):
+        x = findINgroup(filters[i, :], filters[i+1:, :])
+        if x is not False:
+            # y(x) should be low
+            ys = filters[i, x]
+            ratio = ys / filters[i, :].max()
+            stay_idx = torch.nonzero((ratio < 0.5) & (ratio > 0.1) & (ys > 0.05)).flatten()
+            xs.append(x[stay_idx])
+
+    xs = torch.cat(xs)
+    x, y = torch.unique(xs, return_counts=True)
+
+    return x, y
 
 
 def main():
@@ -57,6 +81,9 @@ def main():
         # summean = sumall.mean()
         # summax = sumall.max()
         # loss = (summean - 0.5).abs() * 0.1 + (summean - summax).abs()
+        x, y = get_intersection_dist(filters)
+        intra_var = torch.var(y.float())
+        loss += intra_var
 
 
         reg = (torch.log(indicator) + torch.log(1 - indicator)).mean()
@@ -70,7 +97,7 @@ def main():
         pbar.set_description(
             f'Iteration {iteration:05d}:'
             + f' loss = {loss.detach().item()}'
-            # + f' summean = {summean.detach().item()}'
+            + f' intra_var = {intra_var.detach().item()}'
             + f' reg = {reg.detach().item()}'
             + f' regnum = {regnum.detach().item()}'
         )
